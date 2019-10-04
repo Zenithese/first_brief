de7b9d481fe3b54d7cf4f3bcb0ad14eed8caebc0
Justin <jandersen770@gmail.com>
2019-10-04 00:04:24 -0400

    Add custom right click drop down

--- src/folder.js
+++ src/folder.js
@@ -53,6 +53,42 @@ function renameFolder(e) {
     document.getElementById("folder-name").value = e
 }
 
+var i = document.getElementById("menu").style;
+if (document.addEventListener) {
+    document.addEventListener('contextmenu', function (e) {
+        var posX = e.clientX;
+        var posY = e.clientY;
+        menu(posX, posY);
+        e.preventDefault();
+    }, false);
+    document.addEventListener('click', function (e) {
+        i.opacity = "0";
+        setTimeout(function () {
+            i.visibility = "hidden";
+        }, 501);
+    }, false);
+} else {
+    document.attachEvent('oncontextmenu', function (e) {
+        var posX = e.clientX;
+        var posY = e.clientY;
+        menu(posX, posY);
+        e.preventDefault();
+    });
+    document.attachEvent('onclick', function (e) {
+        i.opacity = "0";
+        setTimeout(function () {
+            i.visibility = "hidden";
+        }, 501);
+    });
+}
+
+function menu(x, y) {
+    i.top = y + "px";
+    i.left = x + "px";
+    i.visibility = "visible";
+    i.opacity = "1";
+}
+
 // function component() {
 //     const element = document.createElement('div');
 
