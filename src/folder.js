ebbd873044b2f9ed9271ffa4f7c34f1e822d4dff
Justin <jandersen770@gmail.com>
2019-10-04 00:58:07 -0400

    Make all new folders draggable

--- src/folder.js
+++ src/folder.js
@@ -1,6 +1,6 @@
 dragElement(document.getElementById("folder"));
 
-function dragElement(elmnt, elmntName) {
+function dragElement(elmnt) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     if (document.getElementById(elmnt.id + "-itself")) {
         /* if present, the header is where you move the DIV from:*/
@@ -89,9 +89,33 @@ function menu(x, y) {
     i.opacity = "1";
 }
 
+newFolderNum = 0
+
 function newFolder() {
-    var newFolder = document.createElement("BUTTON");
+    newFolderName = "folder" + String(newFolderNum)
+    newFolderNameItself = newFolderName + "-itself"
+    console.log(newFolderNameItself);
+    var newFolder = document.createElement("div");
+    newFolder.innerHTML =  `<div id=${newFolderNameItself} style="width: 100px;">
+                                <img class="image" src="http://icon-park.com/imagefiles/folder_icon_yellow.png" style="max-width: 100%">
+                            </div>
+                            <form id="folder-name" onsubmit="renameFolder(e)">
+                                <input type="text" name="new_folder" value="New Folder"
+                                    style="background-color: transparent; border: none; font-size: 12px;">
+                            </form>`
+    newFolder.id = newFolderName
+    newFolder.style =  `width: 100px;
+                        height: 120px;
+                        display: flex;
+                        flex-direction: column;
+                        position: absolute;
+                        left: 10%;
+                        top: 200px;
+                        text-align: center;`
     document.body.appendChild(newFolder);
+    dragElement(document.getElementById(newFolderName));
+    newFolderNum += 1;
+    console.log(newFolderNum)
 }
 
 // function component() {
