b13a090ebdb839d711df6468c0396028ead32b46
Justin <jandersen770@gmail.com>
2019-10-06 18:20:00 -0400

    Update background feature

--- src/background.js
+++ src/background.js
@@ -5,9 +5,15 @@ wallpaper.addEventListener("change", function(e) {
 });
 
 function changeBackground(url) {
-    document.body.style = `background: url(${url}) no-repeat center center fixed; 
+    document.body.style = `background-color: indigo;
+                           background: url(${url}) no-repeat center center fixed; 
                            -webkit-background-size: cover;
                            -moz-background-size: cover;
                            -o-background-size: cover;
                            background-size: cover;`
+    wallpaper.style = `display: none`
+}
+
+function wallpaperInput() {
+    wallpaper.style = `display: block`
 }
