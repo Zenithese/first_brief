7bfce375d461cffb52b58dbd20afa5c22d2cd2ea
Justin <jandersen770@gmail.com>
2019-10-17 10:52:45 -0400

    link to drive on file dblclick

--- src/file.js
+++ src/file.js
@@ -5,21 +5,25 @@ inputNum = 0;
 function newFile(name, link) {
     var newFile = document.createElement("div");
     // newFile.id = "name"
-    newFile.innerHTML = `<div id="name-itself" style="width: 100px;">
+    newFile.innerHTML = `<div id="name-itself" style="width: 100px;" ondblclick="linkToDrive('${link}')">
                             <img class="image" src="assets/images/blue_file_icon.png" style="max-width: 100%">
                          </div>
                          <div>
                             <input id="input-${inputNum}" type="text" value=${name}
                                 style="background-color: transparent; border: none; font-size: 12px; color: white; text-shadow: 1px 1px black;">
-                         </div>
-                         <a href=${link} target="_blank">link</a>`
+                         </div>`
     newFile.style = `width: 100px;
                         flex-direction: column;
                         text-align: center;`
+    // newFile.ondblclick = linkToDrive(link);
     document.body.appendChild(newFile);
     dragElement(newFile);
     var input = document.getElementById(`input-${inputNum}`);
     input.addEventListener('input', resizeInput);
     resizeInput.call(input);
     inputNum++
+}
+
+function linkToDrive(link) {
+    window.open(link)
 }
