50ac429750a79344139df83827c43ba1ad289d02
Justin <jandersen770@gmail.com>
2019-10-17 10:35:07 -0400

    Style files

--- src/file.js
+++ src/file.js
@@ -1,23 +1,25 @@
 // blue_file_icon link: http://www.pngall.com/wp-content/uploads/2018/05/Files-High-Quality-PNG.png
 
+inputNum = 0;
+
 function newFile(name, link) {
     var newFile = document.createElement("div");
     // newFile.id = "name"
     newFile.innerHTML = `<div id="name-itself" style="width: 100px;">
                             <img class="image" src="assets/images/blue_file_icon.png" style="max-width: 100%">
-                        </div>
-                        <div>
-                            <input id="testInput" type="text" value=${name}
+                         </div>
+                         <div>
+                            <input id="input-${inputNum}" type="text" value=${name}
                                 style="background-color: transparent; border: none; font-size: 12px; color: white; text-shadow: 1px 1px black;">
-                        </div>
-                        <a href=${link} target="_blank">Click</a>`
+                         </div>
+                         <a href=${link} target="_blank">link</a>`
     newFile.style = `width: 100px;
                         flex-direction: column;
-                        position: absolute;
                         text-align: center;`
     document.body.appendChild(newFile);
     dragElement(newFile);
-    var input = document.getElementById('testInput');
+    var input = document.getElementById(`input-${inputNum}`);
     input.addEventListener('input', resizeInput);
     resizeInput.call(input);
+    inputNum++
 }
